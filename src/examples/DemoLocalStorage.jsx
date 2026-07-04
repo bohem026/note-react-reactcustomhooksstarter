347df63c96b0811bbbc2d9317782bcb5bf162df2
bohem dev <microbin98@gmail.com>
2026-07-04 15:59:10 +0900

    feat: enhance DemoLocalStorage component with input handling and reset functionality

--- src/examples/DemoLocalStorage.jsx
+++ src/examples/DemoLocalStorage.jsx
@@ -1,14 +1,21 @@
-
 import React from 'react';
 import useLocalStorage from '../hooks/useLocalStorage.js';
 
-export default function DemoLocalStorage(){
+export default function DemoLocalStorage() {
+  const [name, setName, reset] = useLocalStorage('name', '');
 
   return (
     <div>
-      <input placeholder="이름 입력" />
-      <button >reset</button>
-      <p>저장된 값: <b>{}</b></p>
+      <input
+        placeholder="이름 입력"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button onClick={reset}>reset</button>
+      <p>
+        저장된 값: <b>{name}</b>
+      </p>
     </div>
   );
 }
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,26 @@
+import { useState, useEffect } from 'react';
 
-import {  } from 'react';
+export default function useLocalStorage(key, initialValue) {
+  const read = () => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item !== null ? JSON.parse(item) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  };
+  const [value, setValue] = useState(read); // 로컬 스토리지의 기존값-> 초기값
 
-export default function useLocalStorage(){
+  // 로컬 스토리지에 값 저장
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
 
+  // 로컬 스토리지에 값 제거
+  const remove = () => {
+    window.localStorage.removeItem(key); // 삭제
+    setValue(initialValue); // 초기화
+  };
+
+  return [value, setValue, remove];
 }
